49a4bde1838b0d65e19ed909a0a2c90673ace2a3
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-07-28 16:53:52 +0200

    Dependencies added : axios and react load spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Audio } from "react-loader-spinner";
+<Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="three-dots-loading"
+  wrapperStyle
+  wrapperClass
+/>;
 
 function App() {
   return (
