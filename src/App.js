d97cca938f288900b6395d7fdd641e4ad9e480b0
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-07-29 21:02:08 +0200

    base level

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Audio } from "react-loader-spinner";
-<Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="green"
-  ariaLabel="three-dots-loading"
-  wrapperStyle
-  wrapperClass
-/>;
+import React from "react";
+import Search from "./search";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Weather App</h1>
+      <Search />
     </div>
   );
 }
-
-export default App;
