8c37380c5d77dcf233dfc2976be391c10a1d5e6a
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-01-29 20:19:10 +0100

    Updated with new api data.

--- src/ScriptFiles/WeatherClient.js
+++ src/ScriptFiles/WeatherClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api_key = "9f60d7631c91fefe3d27ab9b78997410";
 const openweather_url = `https://api.openweathermap.org`;
 const CurrentApi = `${openweather_url}/data/2.5/weather`;
-const OnecallApi = `${openweather_url}/data/2.5/onecall`;
+const ForecastApi = `${openweather_url}/data/2.5/forecast`;
 
 const WeatherClient = {
   getCurrentWeatherByCity: (city, unit, updateWeatherDetails) => {
@@ -20,42 +20,44 @@ const WeatherClient = {
     });
   },
   getForecastWeather: (lat, lon, unit, updateWeatherForecast) => {
-    const api_url = `${OnecallApi}?lat=${lat}&lon=${lon}&units=${unit}&appid=${api_key}`;
+    const api_url = `${ForecastApi}?lat=${lat}&lon=${lon}&units=${unit}&appid=${api_key}`;
     axios.get(api_url).then((response) => {
-      updateWeatherForecast({
-        weeklyReadings: extract_7days_readings(response.data),
-        hourlyReadings: extract_hourly_readings(response.data),
-      });
+      const forecast = {
+        weeklyReadings: extract_5days_readings(response.data),
+        hourlyReadings: extract_3hour_readings(response.data),
+      }
+      console.log(response.data);
+      console.log(forecast);
+      updateWeatherForecast(forecast);
     });
   },
 };
 
-function extract_hourly_readings(data) {
+function extract_3hour_readings(data) {
   let readings = [];
-  let currentHour = new Date(data.current.dt * 1000).getHours();
-  for (let i = 0; i < 24; i++) {
-    let icon = data.hourly[i].weather[0].icon;
+  for (let i = 0; i < 8; i++) {
+    let icon = data.list[i].weather[0].icon;
     let reading = {
-      hour: (i + currentHour) % 24,
+      hour: new Date(data.list[i].dt * 1000).getHours(),
       image: `https://openweathermap.org/img/wn/${icon}@2x.png`,
-      description: data.hourly[i].weather[0].description,
-      temp: Math.round(data.hourly[i].temp),
+      description: data.list[i].weather[0].description,
+      temp: Math.round(data.list[i].main.temp),
     };
     readings.push(reading);
   }
   return readings;
 }
 
-function extract_7days_readings(data) {
+function extract_5days_readings(data) {
   let readings = [];
-  for (let i = 0; i < 8; i++) {
-    let icon = data.daily[i].weather[0].icon;
+  for (let i = 4; i < 40; i += 8) {
+    let icon = data.list[i].weather[0].icon;
     let reading = {
-      date: new Date(data.daily[i].dt * 1000),
+      date: new Date(data.list[i].dt * 1000),
       image: `https://openweathermap.org/img/wn/${icon}@2x.png`,
-      description: data.daily[i].weather[0].description,
-      tempMax: Math.round(data.daily[i].temp.max),
-      tempMin: Math.round(data.daily[i].temp.min),
+      description: data.list[i].weather[0].description,
+      tempMax: Math.round(data.list[i].main.temp_max),
+      tempMin: Math.round(data.list[i].main.temp_min),
     };
     readings.push(reading);
   }
